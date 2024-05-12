79f949f689716888fcc60d19dc5f3bde862f98f3
Pakorn Sukmankong <floksong.001@gmail.com>
2024-05-13 02:36:55 +0700

    feat: learn widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  print('Hello Flutter');
+  const app = MaterialApp(
+    title: "My App",
+    home: Text('Hello Flutter'),
+  );
+  runApp(app);
 }
+
